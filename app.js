fbf87c2dd017137c10a13485bb730051883c9415
Bruno Agnelo <brunoagnelo.almeida@gmail.com>
2023-06-23 11:13:35 -0400

    testes

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
 class App {
-    constructor
+    constructor() {
+        this.app = express();
+        this.middlewares();
+        this.routes();
+    }
     
+    middlewares() {
+        this.app.use(express.json());
+
+        /*Parâmetros de Configuração*/ 
+        this.app.use((req, res, next) => {
+            res.header("Access-Controll-Allow-Origin", "*");
+            res.header("Access-Controll-Allow-Methods", "Get, POST PUT, DELETE")
+            res.header("Access-Controll-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-With");
+            
+            this.app.unsubscribe(cors());
+            next();
+        })
+    }
+
+    routes() {
+        this.app.use(routes());
+    }
 }
 
-module.exports = new App()
+module.exports = new App().app
